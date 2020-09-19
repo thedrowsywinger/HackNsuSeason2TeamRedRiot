c87f26f499946a6be22e007d00243ce5d3ccef49
thedrowsywinger <abdullah.md.sarwar@gmail.com>
2020-09-19 11:55:59 +0600

    adding the routing

--- company_A_management_system/accounts/views.py
+++ company_A_management_system/accounts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+
+def SigningUpCompanyRepresentatives(request):
+
+    return render(request, "accounts/signup.html")
